52b8093086644c4649a568e4d33115171afed01f
dieters <dieters@e503-2-4.cbm.local>
2017-07-27 10:30:57 +0200

    Design update

--- src/de/coolsolutions/controller/Angebote.java
+++ src/de/coolsolutions/controller/Angebote.java
@@ -5,7 +5,7 @@ import java.io.PrintWriter;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.Statement;
- 
+
 import javax.naming.InitialContext;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
