5d19c25b46d632f40e06490a1dc30906a35825b4
dieters <dieters@e503-2-4.cbm.local>
2017-08-08 18:26:46 +0200

    Bla

--- src/de/coolsolutions/view/Offers.java
+++ src/de/coolsolutions/view/Offers.java
@@ -37,7 +37,7 @@ public class Offers extends HttpServlet {
 	 */
 	@SuppressWarnings("resource")
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		// TODO Auto-generated method stub 
 		 
 		Connection conn = null;
 		Statement stmt = null;
